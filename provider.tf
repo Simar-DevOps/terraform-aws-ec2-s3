2209127e2bf99d3bda92b41867cd29b2d0c4cb71
Simar-DevOps <simarpbx3@gmail.com>
2025-09-08 11:18:45 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -21,13 +21,26 @@ terraform {
   required_version = ">= 1.5.0"
 }
 
-# Allow overriding the AWS profile for CI/CD
+# Allow overriding the AWS profile in CI/CD
 variable "aws_profile" {
   type    = string
   default = "simar-dev"
 }
 
+# Toggle for CI so provider doesn't try to talk to AWS
+variable "ci_mode" {
+  type    = bool
+  default = false
+}
+
 provider "aws" {
+  # Use the local profile normally; disable it in CI by passing aws_profile="ignore"
   profile = var.aws_profile != "ignore" ? var.aws_profile : null
   region  = "us-east-1"
+
+  # In CI, skip all AWS checks so no API calls happen
+  skip_credentials_validation = var.ci_mode
+  skip_requesting_account_id  = var.ci_mode
+  skip_region_validation      = var.ci_mode
+  skip_metadata_api_check     = var.ci_mode
 }
